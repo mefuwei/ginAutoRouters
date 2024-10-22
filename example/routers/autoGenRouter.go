1596cae0d3b7c07cffb924e2729b0b7a15862129
wei.fu <wei.fu@bitdeer.com>
2024-10-22 14:35:36 +0800

    add example

---
+++ example/routers/autoGenRouter.go
@@ -0,0 +1,15 @@
+// Package routers is a package for routing the requests to the appropriate handler functions.
+package routers
+
+import (
+	controllers "autorouters/example/controllers"
+	gin "github.com/gin-gonic/gin"
+)
+
+func RegisterRoutes(routerGroup *gin.RouterGroup) {
+	api := controllers.Api{}
+	// GetApi  Summary
+	routerGroup.GET("/api/v1/get/api/", api.GetApis)
+	// GetApiList Summary
+	routerGroup.POST("/api/v1/GetApiList/", api.GetApiList)
+}
